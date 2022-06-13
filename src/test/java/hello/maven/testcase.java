b8d40249931e3f31e475da48e465bd264e348517
Rakesh <Rakeshgupta.rg26@gmail.coom>
2022-06-13 23:07:04 +0530

    first commit

---
+++ src/test/java/hello/maven/testcase.java
@@ -0,0 +1,17 @@
+package hello.maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class testcase {
+
+	@Test
+	public void shouldAnswerWithTrue()
+	{
+//		fail("Not yet implemented");
+		
+		assertTrue( true );
+	}
+
+}
